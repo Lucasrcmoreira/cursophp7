2fa6610e06e115d63fbc269cb21755c96de30d65
Lucas Ramos <lucasrcmmadruga@gmail.com>
2018-10-15 18:13:59 -0300

    métodos crud e login visualizando em listas json

--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,28 @@
 
 require_once("config.php");
 
-$buscarUsuario = new Usuario();
 
-$buscarUsuario->loadbyId(6);
+// metodo que carrega apenas um usuario
+//$buscarUsuario = new Usuario();
+//$buscarUsuario->selectUsuario(6);
+//echo $buscarUsuario;
 
-echo $buscarUsuario;
 
+
+//método que  carrega uma lista de todos usuarios
+//$listaUsuarios = Usuario::getList();
+//echo json_encode($listaUsuarios);
+
+
+// método que carrega uma lista de usuarios buscado pelo login
+//$buscarUsuarioLogin = Usuario::search("us");
+
+//echo json_encode($buscarUsuarioLogin);
+
+
+//método que carrega usuário usando login e a senha
+$usuario = new Usuario();
+$usuario->login("macada","1239000") ;
+
+echo $usuario;
 ?>
